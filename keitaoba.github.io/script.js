fa328c1c9f96932a1ac79091de52339227be3d56
keitaoba <30496994+keitaoba@users.noreply.github.com>
2017-07-27 21:10:54 +0900

    Add files via upload

---
+++ keitaoba.github.io/script.js
@@ -0,0 +1,10 @@
+$('.button').on('click',function(){
+     $('.button-box').slideToggle();
+});
+$('.top-menu-item-mobile').on('click',function(){
+     $('.box-menu-up').hide();
+});
+$('.box-menu').on('click',function(){
+     $('.box-menu-up').slideToggle();
+});
+
